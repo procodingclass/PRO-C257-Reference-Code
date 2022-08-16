6f641e14166dc2abd4ea31dd2935d8be5ec1aa69
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-08-16 11:59:37 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -18,6 +18,8 @@ ezButton button5(26);
 
 int vote1 = 0, vote2 = 0, vote3 = 0, vote4 = 0;
 
+int flag = 0;
+
 void setup()
 {
   Serial.begin(9600);
@@ -35,23 +37,15 @@ void setup()
 
   delay(2000);         // wait two seconds for initializing
   oled.clearDisplay(); // clear display
-
-  oled.setTextSize(1);         // set text size
+  oled.setTextSize(2);         // set text size
   oled.setTextColor(WHITE);    // set text color
-  oled.clearDisplay();
 
-  oled.setCursor(2, 5);       // set position to display
-  oled.println("Start voting.."); // set text
-  oled.setCursor(2, 15);       // set position to display
-  oled.println("A - Green ");
-  oled.setCursor(2, 25);       // set position to display
-  oled.println("B - Yellow");
-  oled.setCursor(2, 35);       // set position to display
-  oled.println("C - Red");     // display on OLED
-  oled.setCursor(2, 45);       // set position to display
-  oled.println("D - Orange");
-
-  oled.display();              // display on OLED
+  oled.setCursor(2, 5); 
+  oled.println("Start");       // set position to display
+  oled.setCursor(2, 21); 
+  oled.println("voting..");   
+  oled.display();             // display on OLED
+  delay(2000);
 }
 
 void loop() {
@@ -62,6 +56,23 @@ void loop() {
   button4.loop();
   button5.loop();
 
-  
+  if (flag == 0) {
+   // set text
+    oled.clearDisplay(); // clear display
+    oled.setTextSize(2);         // set text size
+    oled.setTextColor(WHITE);    // set text color
+
+    oled.setCursor(2, 0);       // set position to display
+    oled.println("A - Green ");
+    oled.setCursor(2, 16);       // set position to display
+    oled.println("B - Yellow");
+    oled.setCursor(2, 32);       // set position to display
+    oled.println("C - Red");     // display on OLED
+    oled.setCursor(2, 48);       // set position to display
+    oled.println("D - Orange");
+    oled.display();
+    
+  }
+
   delay(10);
 }
